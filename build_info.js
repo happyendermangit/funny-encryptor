8dfd5f38831430edddb963d3801f36ea3c535c69
Happy enderman <66224387+happyendermangit@users.noreply.github.com>
2024-02-20 21:31:26 +0100

    Update build_info.js

--- build_info.js
+++ build_info.js
@@ -1,3 +1,3 @@
 const url = args[0]
-let jsonData = JSON.stringify(JSON.parse(atob(url.replace("https://discord.com/__development/link?s=","").split(".")[1])))
+let jsonData = JSON.stringify(JSON.parse(atob(url.replace("https://discord.com/__development/link?s=","").split(".")[1])),null,4)
 jsonData
